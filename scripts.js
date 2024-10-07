62f7137b5ad7bbd537fe6c7aadebbac9a70d4260
ph2539 <58719221+ph2539@users.noreply.github.com>
2024-10-07 12:41:35 -0400

    Form functionality implemented

--- scripts.js
+++ scripts.js
@@ -17,3 +17,64 @@ document.querySelectorAll('.faq-question').forEach(question => {
         }
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('early-access-form');
+    var submitBtn = document.getElementById('submitBtn');
+    
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        console.log('Form submission intercepted');
+        
+        // Set button to loading state
+        submitBtn.disabled = true;
+        submitBtn.classList.add('loading');
+        
+        var formData = new FormData(form);
+        var jsonData = {};
+        
+        formData.forEach(function(value, key){
+            jsonData[key] = value;
+        });
+        
+        jsonData['Consent'] = document.getElementById('consent-checkbox').checked ? 'Yes' : 'No';
+        
+        console.log('Sending data:', JSON.stringify(jsonData));
+        
+        fetch('https://script.google.com/macros/s/AKfycbyeDpQBvYkudormv-y8SARKH0_5qfNvzhrSugiHDwCWQy0R-S_OCwXEFupKQVH8Q6ud/exec', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jsonData)
+        })
+        .then(response => {
+            console.log('Response received:', response);
+            if (response.type === 'opaque') {
+                showAlertAndReload('Thank you for your submission. We will be in touch soon!');
+            } else {
+                return response.json();
+            }
+        })
+        .then(data => {
+            if (data) {
+                console.log('Response data:', data);
+                if (data.result === "success") {
+                    showAlertAndReload('Thank you for your submission. We will be in touch soon!');
+                } else {
+                    alert('An error occurred: ' + (data.message || 'Unknown error'));
+                }
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again.');
+        })
+    });
+
+    function showAlertAndReload(message) {
+        alert(message);
+        window.location.reload();
+    }
+});
